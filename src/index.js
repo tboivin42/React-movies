e09504732caad7e00cc596dd65e55d6b03a7f6bc
Thibaut Boivin <thibautboivin@MacBook-Pro-de-Thibaut.local>
2018-02-11 06:35:52 +0100

    delete API KEY

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 
 import App from './Containers/App';
 
-const API_KEY = "c54a42a9a49a29240ec7bdc964c87f1c";
-
 render(
   <App/>, 
   document.querySelector('.container')
