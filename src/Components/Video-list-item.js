66d213b09ed83f5cbfe8b4f3255de1d7a5b7232d
Thibaut Boivin <thibautboivin@MacBook-Pro-de-Thibaut.local>
2018-02-11 04:51:41 +0100

    Add image

--- src/Components/Video-list-item.js
+++ src/Components/Video-list-item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
 const VideoListItem = ({movie}) => {
   return (
     <li>
-      <img src="" />
-      <h3>{ movie.title }</h3>
-    </li>)
+      <img height="10%" width="10%" src={`${IMG_BASE_URL}${movie.poster_path}`} />
+      { movie.title }
+    </li>
+  )
 }
 
 export default VideoListItem;
